6d8c5dbbb9be50fe0decf61adad6e844f9979b72
ElementsiaGov <140907717+ElementsiaGov@users.noreply.github.com>
2023-12-03 18:33:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function checkPassword() {
+    // Get the entered password
+    var enteredPassword = document.getElementById('password').value;
+
+    // Define your secret pages and their corresponding passwords
+    var secretPages = {
+        'Columbia': '19807',
+        'Orianne and Geneva': '12654',
+        'United Empire': '45892',
+        'Minuvano': '74462',
+        'Dauman Republic': '13795',
+        'Sadripoor': '45685',
+        'Nationale Republik': '13467',
+        'Gympieland': '78451',
+        'Vermillion': '32659',
+        'Hallow Carthage': '45120',
+        'Krimera': '56890',
+        'Puritanos Fed': '32598',
+        'Calicadonia': '73015',
+        'Romanic Imperium': '43085',
+        'Isomedia': '96015',
+        'The Outer Rim': '45680',
+        'Nambiadia': '08546',
+        'Dunwich Innsmouth': '40358',
+        'Sianoi Union': '24571',
+        'Siennisia': '78964',
+        'Rerria': '35874',
+        'Jesia': '78541',
+        'Necropolis': '68971',
+        // Add more pages as needed
+    };
+
+    // Check if the entered password corresponds to any secret page
+    for (var page in secretPages) {
+        if (enteredPassword === secretPages[page]) {
+            window.location.href = page + '.html'; // Redirect to the corresponding secret page
+            return;
+        }
+    }
+
+    // If the password doesn't match any secret page, show an alert
+    alert('Incorrect password. Please try again.');
+}
---
+++ secret-page.php
@@ -0,0 +1,15 @@
+<?php
+// This page is accessible only if the correct password is entered
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secret Page</title>
+</head>
+<body>
+    <h1>Congratulations! You've reached the secret page.</h1>
+</body>
+</html>
